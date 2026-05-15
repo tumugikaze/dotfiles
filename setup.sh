04ca92cea01ab90d9f3399a8d43722c8c8920ce7
tumugikaze <tumugikaze@gmail.com>
2026-05-16 02:24:03 +0900

    fix: Update Node installation command in setup script

--- setup.sh
+++ setup.sh
@@ -178,7 +178,7 @@ fi
 # --- Node ---
 if ! exists volta; then
     echo "Installing Node(volta)..."
-    curl https://get.volta.sh | bash --no-modify-path
+    curl https://get.volta.sh | bash
     export VOLTA_HOME="$HOME/.volta"
     export PATH="$VOLTA_HOME/bin:$PATH"
     volta install node
