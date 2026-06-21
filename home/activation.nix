8024325006f0eca7bf507fd220580498f6ff340c
tumugikaze <tumugikaze@gmail.com>
2026-06-22 01:58:07 +0900

    Fix Nix

--- home/activation.nix
+++ home/activation.nix
@@ -4,7 +4,7 @@
   home.activation = {
 
     installRustup = lib.hm.dag.entryAfter [ "writeBoundary" ] ''
-      export PATH="/usr/local/bin:/usr/bin:/bin:$HOME/.nix-profile/bin:$PATH"
+      export PATH="/usr/local/bin:/usr/bin:/bin:$HOME/.nix-profile/bin:$HOME/.cargo/bin:$PATH"
       if ! command -v cargo >/dev/null 2>&1; then
         $DRY_RUN_CMD echo "Installing rustup..."
         $DRY_RUN_CMD curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs \
@@ -13,7 +13,7 @@
     '';
 
     installVolta = lib.hm.dag.entryAfter [ "writeBoundary" ] ''
-      export PATH="/usr/local/bin:/usr/bin:/bin:$HOME/.nix-profile/bin:$PATH"
+      export PATH="/usr/local/bin:/usr/bin:/bin:$HOME/.nix-profile/bin:$HOME/.volta/bin:$PATH"
       if ! command -v volta >/dev/null 2>&1; then
         $DRY_RUN_CMD echo "Installing volta..."
         $DRY_RUN_CMD curl https://get.volta.sh | bash -s -- --skip-setup
@@ -24,7 +24,7 @@
     '';
 
     installUv = lib.hm.dag.entryAfter [ "writeBoundary" ] ''
-      export PATH="/usr/local/bin:/usr/bin:/bin:$HOME/.nix-profile/bin:$PATH"
+      export PATH="/usr/local/bin:/usr/bin:/bin:$HOME/.nix-profile/bin:$HOME/.local/bin:$PATH"
       if ! command -v uv >/dev/null 2>&1; then
         $DRY_RUN_CMD echo "Installing uv..."
         $DRY_RUN_CMD curl -LsSf https://astral.sh/uv/install.sh \
@@ -33,7 +33,7 @@
     '';
 
     installNodePackages = lib.hm.dag.entryAfter [ "installVolta" ] ''
-      export PATH="/usr/local/bin:/usr/bin:/bin:$HOME/.nix-profile/bin:$PATH"
+      export PATH="/usr/local/bin:/usr/bin:/bin:$HOME/.nix-profile/bin:$HOME/.volta/bin:$PATH"
       export VOLTA_HOME="$HOME/.volta"
       export PATH="$VOLTA_HOME/bin:$PATH"
       if command -v volta >/dev/null 2>&1; then
